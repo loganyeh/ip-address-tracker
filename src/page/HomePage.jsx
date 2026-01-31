16c8ea9b6b9e4ad2553789109ac2b9658e087a02
loganyeh <yehlogan@gmail.com>
2026-01-30 23:12:03 -0500

    wip

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,14 @@
+
+function HomePage(){
+
+    return(
+        <>
+            <div className="border-2 border-red-600 h-screen w-screen">
+                <img src="public/images/pattern-bg-desktop.png" alt="" className="h-1/3 w-full"/>
+                <div className="border-2 border-black h-2/3 w-full"></div>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
